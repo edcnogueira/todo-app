2850c42915d69ebca4c152504911d4aceb200bce
Edjancarlos <edjan.nogueira14@gmail.com>
2024-02-21 13:08:05 -0300

    feat: add functional example to htmx and templ

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/edcnogueira/todo-app
 
 go 1.21.2
 
-require github.com/a-h/templ v0.2.543 // indirect
+require github.com/a-h/templ v0.2.543
--- main.go
+++ main.go
@@ -2,19 +2,48 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
-
-	"github.com/a-h/templ"
 )
 
+type GlobalState struct {
+	Count int
+}
+
+var global GlobalState
+
+func getHandler(w http.ResponseWriter, r *http.Request) {
+	component := counts(global.Count, 0)
+	component.Render(r.Context(), w)
+}
+
+func postHandler(w http.ResponseWriter, r *http.Request) {
+	// Update state.
+	r.ParseForm()
+
+	// Check to see if the global button was pressed.
+	if r.Form.Has("global") {
+		global.Count++
+	}
+	//TODO: Update session.
+
+	// Display the form.
+	getHandler(w, r)
+}
+
 func main() {
-	component := hello("John")
+	// Handle POST and GET requests.
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			postHandler(w, r)
+			return
+		}
+		getHandler(w, r)
+	})
 
-	http.Handle("/", templ.Handler(component))
-
-	fmt.Println("Listening on :3000")
-	err := http.ListenAndServe(":3000", nil)
-	if err != nil {
-		return
+	// Start the server.
+	fmt.Println("listening on http://localhost:8000")
+	if err := http.ListenAndServe("localhost:8000", nil); err != nil {
+		log.Printf("error listening: %v", err)
 	}
 }
